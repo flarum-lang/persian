11c18640e38539554b3c12c6dc37e27907a39d34
Hamed <hamed.mmc@gmail.com>
2024-11-19 22:51:14 +0330

    Translate fixes

--- locale/config.js
+++ locale/config.js
@@ -1,12 +1,7 @@
-// وارد کردن کتابخانه dayjs و پلاگین شمسی
-import dayjs from 'dayjs';
-import jalali from 'dayjs-jalali';
+const dayjs = require('dayjs');
+const jalali = require('dayjs-jalali');
 
-// اضافه کردن پلاگین شمسی به dayjs
 dayjs.extend(jalali);
-
-// تنظیم زبان فارسی برای dayjs
 dayjs.locale('fa');
 
-// تست تاریخ شمسی
-console.log(dayjs().format('YYYY/MM/DD')); // نمایش تاریخ شمسی
+console.log(dayjs().format('YYYY/MM/DD'));
